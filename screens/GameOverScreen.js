602ce596a07067cefb210876487face041ce6dc8
Hari Prayudha <prayudahari@gmail.com>
2025-01-26 08:14:33 +0700

    Update STyle

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
         fontFamily: 'open-sans',
         fontSize: 24,
         textAlign: 'center',
-        marginVertical: 24
+        marginVertical: 23
     },
     highlight: {
         fontFamily: 'open-sans-bold',
